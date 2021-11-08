7d84a563d58799fde6582a805a9b36b3cef1721f
Zehua Zou <41586196+HuaHuaY@users.noreply.github.com>
2021-11-08 13:06:02 +0800

    add shell sort

---
+++ src/algorithm/sort/shell_sort/mod.rs
@@ -0,0 +1,50 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn shell_sort(nums: &mut [i32]) {
+        let mut gap = nums.len() / 2;
+        while gap > 0 {
+            for i in gap..nums.len() {
+                let mut p = i;
+                while p >= gap && nums[p] < nums[p - gap] {
+                    nums.swap(p, p - gap);
+                    p -= gap;
+                }
+            }
+            gap /= 2;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let mut nums = [2, 4, 1, 3, 5];
+        Solution::shell_sort(&mut nums);
+        assert_eq!([1, 2, 3, 4, 5], nums);
+    }
+
+    #[test]
+    fn test2() {
+        let mut nums = [1, 2, 0, 0, 2];
+        Solution::shell_sort(&mut nums);
+        assert_eq!([0, 0, 1, 2, 2], nums);
+    }
+
+    #[test]
+    fn test3() {
+        let mut nums: [i32; 0] = [];
+        Solution::shell_sort(&mut nums);
+        assert_eq!([] as [i32; 0], nums);
+    }
+
+    #[test]
+    fn test4() {
+        let mut nums = [0];
+        Solution::shell_sort(&mut nums);
+        assert_eq!([0], nums);
+    }
+}
