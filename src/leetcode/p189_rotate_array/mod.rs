97264ad0221515a5fd398e186ce77a89bf8f695b
Zehua Zou <41586196+HuaHuaY@users.noreply.github.com>
2024-10-25 20:43:58 +0800

    fix clippy

--- src/leetcode/p189_rotate_array/mod.rs
+++ src/leetcode/p189_rotate_array/mod.rs
@@ -1,6 +1,7 @@
 pub struct Solution;
 
 impl Solution {
+    #[allow(clippy::ptr_arg)]
     pub fn rotate(nums: &mut Vec<i32>, k: i32) {
         let len = nums.len();
         nums.rotate_right(k as usize % len);
